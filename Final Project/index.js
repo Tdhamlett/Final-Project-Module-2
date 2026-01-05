b066a375a7e5c335adbfe202bb32b0a1cd320675
Tdhamlett <hamletttravis@yahoo.com>
2026-01-05 14:18:06 -0500

    git push

--- Final Project/index.js
+++ Final Project/index.js
@@ -1,6 +1,6 @@
-function openMeunu() {
+function openMenu() {
     document.body.classList.add("menu--open");
-        }
+}
 
 function closeMenu() {
     document.body.classList.remove("menu--open");
---
+++ JavaScript/app.js
@@ -0,0 +1,13 @@
+function showRating(rating) {
+    let ratings = ""
+    for (let i = 0; i = 4; ++i) {
+        ratings = ratings + "*"
+        if (i === 3) {
+            ratings = ratings + "*"
+
+        }
+    }
+    return ratings
+}
+
+console.log(showRating(2));
